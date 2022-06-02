c513ebae4d1414114f04fd693150cba72e0ef067
ABDO1632 <43861189+ABDO1632@users.noreply.github.com>
2022-06-02 14:01:46 +0100

    question 2

---
+++ app/src/main/java/com/example/exam/Entreprise.java
@@ -0,0 +1,48 @@
+package com.example.exam;
+
+public class Entreprise {
+    private int ID;
+    private String RaisonSociale,Adresse;
+    private double Capitale;
+    public Entreprise() {
+
+    }
+    public Entreprise(int ID, String raisonSociale, String adresse, double capitale) {
+        this.ID = ID;
+        RaisonSociale = raisonSociale;
+        Adresse = adresse;
+        Capitale = capitale;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getRaisonSociale() {
+        return RaisonSociale;
+    }
+
+    public void setRaisonSociale(String raisonSociale) {
+        RaisonSociale = raisonSociale;
+    }
+
+    public String getAdresse() {
+        return Adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        Adresse = adresse;
+    }
+
+    public double getCapitale() {
+        return Capitale;
+    }
+
+    public void setCapitale(double capitale) {
+        Capitale = capitale;
+    }
+}
