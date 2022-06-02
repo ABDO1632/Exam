60a437a8f556e00585e41a71c548487cb37868c8
ABDO1632 <43861189+ABDO1632@users.noreply.github.com>
2022-06-02 16:34:37 +0100

    question 12

--- app/src/main/java/com/example/exam/ListerPage.java
+++ app/src/main/java/com/example/exam/ListerPage.java
@@ -3,12 +3,40 @@ package com.example.exam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ListerPage extends AppCompatActivity {
+    ListView lst;
+    MyDatabase db;
+    ArrayList<Entreprise> sts;
+    Entreprise en;
+    ArrayAdapter ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lister_page);
+        lst=findViewById(R.id.listViewE);
+        db=new MyDatabase(this);
+        //Load List View
+        sts = MyDatabase.getAllEntreprise(db.getReadableDatabase());
+
+        ArrayList<HashMap<String,Object>> lstItems=new ArrayList<>();
+        for(Entreprise c : sts){
+            HashMap<String,Object> it=new HashMap<String,Object>();
+            it.put("txtRs",c.getRaisonSociale());
+            it.put("txC",c.getCapitale());
+            lstItems.add(it);
+            //nomPays.add(c.getName());
+        }
+        String[] from={"txtRs","txC"};
+        int[] to={R.id.RSLineStructure,R.id.textCapitalLineStructure};
+        SimpleAdapter ad = new SimpleAdapter(this,lstItems,R.layout.line_structure,from,to);
+        lst.setAdapter(ad);
     }
 }
